1ef4cf02ba681dcfaa0d76bea955ef5fa86b261d
John Gardiner <JohnGardiner93@users.noreply.github.com>
2021-02-18 18:06:20 -0500

    2/18/21 Coursework

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -263,45 +263,154 @@ BONUS TEST DATA 1: [5, 2, 3]
 BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 
 GOOD LUCK 😀
-*/
 
 const poll = {
-  question: `What is your favourite programming language?`,
-  options: [`0: JavaScript`, `1: Python`, `2: Rust`, `3: C++`],
-  // This generates [0,0,0,0]. More in the next section.
-  answers: new Array(4).fill(0),
-  registerNewAnswer: function () {
-    const answer = Number(
-      prompt(
-        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+question: `What is your favourite programming language?`,
+options: [`0: JavaScript`, `1: Python`, `2: Rust`, `3: C++`],
+// This generates [0,0,0,0]. More in the next section.
+answers: new Array(4).fill(0),
+registerNewAnswer: function () {
+  const answer = Number(
+    prompt(
+      `${this.question}\n${this.options.join('\n')}\n(Write option number)`
       )
-    );
-
-    answer < this.answers.length && answer >= 0
+      );
+      
+      answer < this.answers.length && answer >= 0
       ? this.answers[answer]++
       : console.log(`Not a valid number`);
-    displayResults.call(this.answers);
-  },
-};
-
-document
+      displayResults.call(this.answers);
+    },
+  };
+  
+  document
   .querySelector(`.poll`)
   .addEventListener(`click`, poll.registerNewAnswer.bind(poll));
-
-// console.log(poll.answers);
-
-const displayResults = function (type = `array`) {
-  if (type.toLowerCase() === 'array') {
-    console.log(this);
-  } else if (type.toLowerCase() === `string`) {
-    const message = `Poll results are ${this.join(`, `)}`;
-    console.log(message);
-  } else {
-    console.log(`Who am I...`);
+  
+  // console.log(poll.answers);
+  
+  const displayResults = function (type = `array`) {
+    if (type.toLowerCase() === 'array') {
+      console.log(this);
+    } else if (type.toLowerCase() === `string`) {
+      const message = `Poll results are ${this.join(`, `)}`;
+      console.log(message);
+    } else {
+      console.log(`Who am I...`);
+    }
+  };
+  
+  displayResults.call([5, 2, 3], `string`);
+  displayResults.call([5, 2, 3]);
+  displayResults.call([1, 5, 3, 9, 6, 1], `string`);
+  displayResults.call([1, 5, 3, 9, 6, 1]);
+  
+  
+  ////////////////////////////////////////////
+  // Immediately Invoked Function Expressions (IIFE)
+  const runOnce = function () {
+    console.log(`This will never run again`);
+  };
+  
+  runOnce();
+  
+  // IIFE
+  (function () {
+    console.log(`This will never run again`);
+  })();
+  
+  (() => console.log(`This will also never run again`))();
+  
+  {
+    const isPrivate = 23;
+    var notPrivate = 46;
   }
-};
+  
+  // console.log(isPrivate);
+  console.log(notPrivate);
+    
+  
+  ////////////////////////////////////////////
+  // Closures
+  const secureBooking = function () {
+    let passengerCount = 0;
+    
+    return function () {
+      passengerCount++;
+      console.log(`${passengerCount} passengers`);
+    };
+  };
+  
+  const booker = secureBooking();
+  
+  booker();
+  booker();
+  booker();
+  
+  
+  ////////////////////////////////////////////
+  // Closure Examples
+  // Example 1
+  let f;
+  
+  const g = function () {
+    const a = 23;
+    f = function () {
+      console.log(a * 2);
+    };
+  };
+  
+  const h = function () {
+    const b = 777;
+    f = function () {
+      console.log(b * 2);
+    };
+  };
+  
+  g();
+  f();
+  console.dir(f);
+  
+  // Reassign f function
+  h();
+  f();
+  console.dir(f);
+  
+  // Example 2
+  const boardPassengers = function (n, wait) {
+    const perGroup = n / 3;
+    
+    setTimeout(function () {
+      console.log(`We are now boarding all ${n} passengers`);
+      console.log(`There are 3 groups, each with ${perGroup} passengers`);
+    }, wait * 1000);
+    
+    console.log(`Will start boarding in ${wait} seconds`);
+  };
+  
+  const perGroup = 1000;
+  
+  boardPassengers(180, 3);
+  
+*/
 
-displayResults.call([5, 2, 3], `string`);
-displayResults.call([5, 2, 3]);
-displayResults.call([1, 5, 3, 9, 6, 1], `string`);
-displayResults.call([1, 5, 3, 9, 6, 1]);
+////////////////////////////////////////////
+// Coding Challenge #2
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = `red`;
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = `blue`;
+  });
+})();
