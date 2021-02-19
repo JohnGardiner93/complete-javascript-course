be20949976a6c3aa8553b4197e5f9de82ec93110
John Gardiner <JohnGardiner93@users.noreply.github.com>
2021-02-19 12:54:22 -0500

    2/19/21 Coursework

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,19 +61,34 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? `deposit` : `withdrawal`;
+
+    const html = `
+  <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    <div class="movements__value">${mov}</div>
+  </div>
+  `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
+// console.log(containerMovements.innerHTML);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+// Simple Array Methods
+/*
 let arr = [`a`, `b`, `c`, `d`, `e`];
 
 // SLICE
@@ -106,3 +121,93 @@ console.log(...arr, ...arr2);
 
 // JOIN
 console.log(letters.join(' - '));
+
+////////////////////////////////////////////
+// Looping Arrays: forEach
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+  for (const [i, movement] of movements.entries()) {
+    if (movement > 0) {
+      console.log(`Movement ${i + 1}: You deposited ${movement}`);
+    } else {
+      console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+    }
+  }
+  console.log(`\n---- FOREACH ----`);
+  
+  movements.forEach(function (mov, i, arr) {
+    if (mov > 0) {
+      console.log(`Movement ${i + 1}: You deposited ${mov}`);
+    } else {
+      console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+    }
+  });
+  
+  // 0: function(200)
+  // 1: function(450)...
+  
+  
+  ////////////////////////////////////////////
+  // ForEach with Maps and Sets
+  // Map
+  const currencies = new Map([
+    ['USD', 'United States dollar'],
+    ['EUR', 'Euro'],
+    ['GBP', 'Pound sterling'],
+  ]);
+  
+  currencies.forEach(function (value, key, map) {
+    console.log(`${key}: ${value}`);
+  });
+  
+  // Set
+  const currenciesUnique = new Set([`USD`, `GBP`, `USD`, `EUR`, `EUR`]);
+  console.log(currenciesUnique);
+  currenciesUnique.forEach(function (value, _, map) {
+    console.log(`${value}: ${value}`);
+  });
+  
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+// Data Set 1
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCopy = dogsJulia.slice().slice(1, -2);
+  // console.log(dogsJuliaCopy);
+  const allDogs = dogsJuliaCopy.concat(dogsKate);
+  console.log(allDogs);
+  allDogs.forEach(function (dog, i) {
+    console.log(`Dog number ${i + 1} is a ${dog >= 3 ? `adult` : `puppy`}`);
+  });
+};
+
+let dogsJulia = [3, 5, 2, 12, 7];
+let dogsKate = [4, 1, 15, 8, 3];
+
+checkDogs(dogsJulia, dogsKate);
+
+dogsJulia = [9, 16, 6, 8, 3];
+dogsKate = [10, 5, 6, 1, 4];
+
+checkDogs(dogsJulia, dogsKate);
