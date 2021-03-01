d037c099fcd6c84f979b8baf541424d572865fc9
John Gardiner <JohnGardiner93@users.noreply.github.com>
2021-03-01 16:35:59 -0500

    3/1/21 Morning Coursework

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,98 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/*
+ */
+
+////////////////////////////////////////////
+////////////////////////////////////////////
+// Lectures
+////////////////////////////////////////////
+// Selecting, Creating, and Deleting Elements
+// Selecting Elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector(`.header`);
+const allSections = document.querySelectorAll(`.section`);
+
+console.log(allSections);
+
+document.getElementById(`section--1`);
+const allButtons = document.getElementsByTagName(`button`);
+console.log(allButtons);
+
+console.log(document.getElementsByClassName(`btn`));
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+
+const message = document.createElement(`div`);
+
+message.classList.add(`cookie-message`);
+// message.textContent = `We use cookies for improved functionality and analytics.`
+message.innerHTML = `We use cookies for improved functionality and analytics.<button class="btn btn--close-cookie">Got it!</button>`;
+
+// header.prepend(message);
+header.append(message);
+
+// header.append(message.cloneNode(true));
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document
+  .querySelector(`.btn--close-cookie`)
+  .addEventListener(`click`, function () {
+    message.parentElement.removeChild(message);
+  });
+
+////////////////////////////////////////////
+// Styles, Attributes, and Classes
+// Styles
+message.style.backgroundColor = `#37383d`;
+message.style.width = `120%`;
+
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 40 + `px`;
+
+document.documentElement.style.setProperty(`--color-primary`, `orangered`);
+
+// Attributes
+const logo = document.querySelector(`.nav__logo`);
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = `Beautiful minimalist logo`;
+
+console.log(logo.designer); // Non-standard, doesn't work
+
+console.log(logo.getAttribute(`designer`));
+logo.setAttribute(`company`, `Bankist`);
+
+console.log(logo.src);
+console.log(logo.getAttribute(`src`));
+
+const link = document.querySelector(`.nav__link--btn`);
+console.log(link.href);
+console.log(link.getAttribute(`href`));
+
+// Data Attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add(`c`, `j`);
+logo.classList.remove(`c`, `j`);
+logo.classList.toggle(`c`);
+logo.classList.contains(`c`);
+
+// DON"T USE THIS - OVERWRITES ALL EXISTING CLASSES
+// logo.className = `jonas`;
