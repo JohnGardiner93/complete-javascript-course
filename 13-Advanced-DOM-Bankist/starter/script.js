1168a4969cee65638db72019c2d08b3c57275d70
John Gardiner <JohnGardiner93@users.noreply.github.com>
2021-03-03 07:03:02 -0500

    3/3/21 Coursework - Morning
    Section 13
    - A Better Way: The Intersection Observer API
    - Revealing Elements On Scroll

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -139,14 +139,76 @@ const handleHover = function (e, opacity) {
 nav.addEventListener(`mouseover`, handleHover.bind(0.5));
 nav.addEventListener(`mouseout`, handleHover.bind(1));
 
+////////////////////////////////////////////
 // Sticky Navigation
-const initialCoords = section1.getBoundingClientRect();
+// const initialCoords = section1.getBoundingClientRect();
 
-window.addEventListener(`scroll`, function () {
-  console.log(window.scrollY);
+// window.addEventListener(`scroll`, function () {
+//   // console.log(window.scrollY);
 
-  if (window.scrollY > initialCoords.top) nav.classList.add(`sticky`);
-  else nav.classList.add(`sticky`);
+//   if (window.scrollY > initialCoords.top) nav.classList.add(`sticky`);
+//   else nav.classList.add(`sticky`);
+// });
+
+////////////////////////////////////////////
+// Sticky Navigation: Intersection Observer API
+
+// Testing
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => console.log(entry));
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
+const header = document.querySelector(`.header`);
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    nav.classList.add(`sticky`);
+  } else {
+    nav.classList.remove(`sticky`);
+  }
+};
+
+const heasderObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+heasderObserver.observe(header);
+
+////////////////////////////////////////////
+// Revealing Elements in Scroll
+// Reveal Sections
+
+const allSections = document.querySelectorAll(`.section`);
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove(`section--hidden`);
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add(`section--hidden`);
 });
 
 /*
