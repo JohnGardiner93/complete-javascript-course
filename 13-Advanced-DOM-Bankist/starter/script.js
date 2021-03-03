36683b7dd57e708e12a4e830b3bdd0bb94182cb6
John Gardiner <JohnGardiner93@users.noreply.github.com>
2021-03-02 19:34:36 -0500

    Coursework 3/2/21 Evening
    - Menu Fade Animation
    - Passing an "argument" into an event handler
    - Sticky Navigation
    -Removed "Movement" folder. Starting new project.

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -9,6 +9,10 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector(`.btn--scroll-to`);
 const section1 = document.querySelector(`#section--1`);
+const nav = document.querySelector(`.nav`);
+const tabs = document.querySelectorAll(`.operations__tab`);
+const tabsContainer = document.querySelector(`.operations__tab-container`);
+const tabsContent = document.querySelectorAll(`.operations__content`);
 
 const openModal = function (e) {
   e.preventDefault();
@@ -95,9 +99,6 @@ document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
 });
 
 // Tabbed Components
-const tabs = document.querySelectorAll(`.operations__tab`);
-const tabsContainer = document.querySelector(`.operations__tab-container`);
-const tabsContent = document.querySelectorAll(`.operations__content`);
 
 // tabs.forEach(t => t.addEventListener(`click`, () => console.log(`TAB`)));
 
@@ -118,6 +119,34 @@ tabsContainer.addEventListener(`click`, function (e) {
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add(`operations__content--active`);
+});
+
+// Menu Fade Animation
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains(`nav__link`)) {
+    const link = e.target;
+    const siblings = link.closest(`.nav`).querySelectorAll(`.nav__link`);
+    const logo = link.closest(`.nav`).querySelector(`img`);
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing an "argument" into handler
+nav.addEventListener(`mouseover`, handleHover.bind(0.5));
+nav.addEventListener(`mouseout`, handleHover.bind(1));
+
+// Sticky Navigation
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener(`scroll`, function () {
+  console.log(window.scrollY);
+
+  if (window.scrollY > initialCoords.top) nav.classList.add(`sticky`);
+  else nav.classList.add(`sticky`);
 });
 
 /*
