72f793a6da6e5595f373e481633d1c38e00cc567
John Gardiner <JohnGardiner93@users.noreply.github.com>
2021-03-04 07:19:07 -0500

    Coursework 3/4/21 Morning
    Section 13
    - Lazy Loading Images
    - Building a Sliding Component: Part 1

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -194,7 +194,6 @@ const allSections = document.querySelectorAll(`.section`);
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
   if (!entry.isIntersecting) return;
 
   entry.target.classList.remove(`section--hidden`);
@@ -208,8 +207,84 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add(`section--hidden`);
+  // section.classList.add(`section--hidden`);
 });
+
+////////////////////////////////////////////
+// Lazy loading images
+const imgTargets = document.querySelectorAll(`img[data-src]`);
+console.log(imgTargets);
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener(`load`, function () {
+    entry.target.classList.remove(`lazy-img`);
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-200px`,
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+////////////////////////////////////////////
+// Slider
+const slides = document.querySelectorAll(`.slide`);
+const btnLeft = document.querySelector(`.slider__btn--left`);
+const btnRight = document.querySelector(`.slider__btn--right`);
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+// const slider = document.querySelector(`.slider`);
+// slider.style.transform = `scale(0.2) translateX(-1000px)`;
+// slider.style.overflow = `visible`;
+
+slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+// 0%, 100%, 200%< 300%
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - curSlide)}%)`)
+  );
+};
+
+// Initialize
+goToSlide(0);
+
+// Next Slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) curSlide = 0;
+  else {
+    curSlide++;
+  }
+
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+// Basically, channging % of transform value
+btnRight.addEventListener(`click`, nextSlide);
+btnLeft.addEventListener(`click`, prevSlide);
 
 /*
 // Lectures
