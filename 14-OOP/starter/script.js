98cdd139a4ab825d8a452eaf6a5f372d8ef78f61
John Gardiner <JohnGardiner93@users.noreply.github.com>
2021-03-10 07:49:01 -0500

    3/10/21 Coursework - Morning
    Section 14
    - Coding Challenge #1
    - ES6 Classes
    - Setters and Getters
    - Static Methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -28,6 +28,14 @@ console.log(matilda, jack);
 
 console.log(jonas instanceof Person);
 
+Person.hey = function () {
+  console.log(`Hey there`);
+  console.log(this);
+};
+
+Person.hey();
+/*
+
 ////////////////////////////////////////////
 // Prototypes
 console.log(Person.prototype);
@@ -76,3 +84,138 @@ console.log(arr.unique());
 const h1 = document.querySelector(`h1`);
 console.dir(h1);
 console.dir(x => x + 1);
+*/
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+
+// My solution
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`The ${this.make} is now going ${this.speed} km/h.`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`The ${this.make} is now going ${this.speed} km/h.`);
+};
+
+const car1 = new Car(`BMW`, 120);
+const car2 = new Car(`Mercedes`, 95);
+
+car1.accelerate();
+car1.accelerate();
+car1.accelerate();
+car1.brake();
+car1.brake();
+
+car2.accelerate();
+car2.accelerate();
+car2.accelerate();
+car2.brake();
+car2.brake();
+*/
+
+////////////////////////////////////////////
+// ES6 Classes
+
+// Class Expression
+// const PersonCl = class {}
+
+// Class Declaration
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Instance methods
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    if (name.includes(` `)) this._fullName = name;
+    else alert(`${name} is not a full name`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  // Static method
+  static hey() {
+    console.log(`Hey there`);
+    console.log(this);
+  }
+}
+
+const jessica = new PersonCl(`Jessica Davis`, 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class citizens
+// 3. Classes are executed in strict mode
+
+const walter = new PersonCl(`Walter White`, 1965);
+console.log(walter);
+
+PersonCl.hey();
+
+////////////////////////////////////////////
+// Setters and Getters
+
+const account = {
+  owner: `jonas`,
+  movemements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movemements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movemements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 60;
+
+console.log(account.movemements);
+
+// Static Methods
