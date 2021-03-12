7758b1afdc49f6b871b5730928412c3cb61d681b
John Gardiner <JohnGardiner93@users.noreply.github.com>
2021-03-12 07:56:37 -0500

    Coursework - 3/12/21
    Section 14
    - Coding Challenge #3
    - Inheritance Between "Classes": ES6 Classes
    - Inheritance Between "Classes": Object.create
    - Another Class Example
    - Encapsulation: Protected Properties and Methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -259,8 +259,6 @@ sarah.calcAge();
 DATA CAR 1: 'Ford' going at 120 km/h
 
 GOOD LUCK 😀
-*/
-/*
 class CarCl {
   constructor(make, speed) {
     this.make = make;
@@ -285,6 +283,9 @@ class CarCl {
     this.speed = speed * 1.6;
   }
 }
+*/
+
+/*
 
 const car = new CarCl(`Ford`, 120);
 
@@ -303,7 +304,7 @@ console.log(car.speed);
 
 ////////////////////////////////////////////
 // Inheritance Between "Classes": Constructor Functions
-
+/*
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
@@ -341,3 +342,211 @@ console.log(mike instanceof Object);
 Student.prototype.constructor = Student;
 
 console.dir(Student.prototype.constructor);
+*/
+////////////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
+
+DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`The ${this.make} is now going ${this.speed} km/h.`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`The ${this.make} is now going ${this.speed} km/h.`);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
+
+const evCar = new EV(`Tesla`, 120, 23);
+
+evCar.chargeBattery(50);
+evCar.accelerate();
+evCar.accelerate();
+evCar.brake();
+*/
+
+/*
+////////////////////////////////////////////
+// Inheritance Between "Classes": ES6 Classes
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Instance methods
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    if (name.includes(` `)) this._fullName = name;
+    else alert(`${name} is not a full name`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  // Static method
+  static hey() {
+    console.log(`Hey there`);
+  }
+}
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    // Always needs to happen first!
+    super(fullName, birthYear, course);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+
+  calcAge() {
+    console.log(
+      `I am ${
+        2037 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
+}
+
+const martha = new StudentCl(`Martha Jones`, 2012, `Computer Science`);
+martha.introduce();
+
+martha.calcAge();
+
+////////////////////////////////////////////
+// Inheritance Between "Classes": Object.create
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+
+jay.init(`Jay`, 2010, `Bean Science`);
+jay.calcAge();
+console.log(jay);
+jay.introduce();
+*/
+
+////////////////////////////////////////////
+// Another Class Example
+// Encapsulation: Protected Properties and Methods
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.locale = navigator.language;
+    // protected property
+    this._movements = [];
+    this._pin = pin;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public Interface
+  getMovements() {
+    return this._movements;
+  }
+
+  deposit(val) {
+    this._movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  // Protected method (internal use only)
+  _approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan Approved`);
+    }
+  }
+}
+
+const acc1 = new Account(`Jonas`, `EUR`, 1111);
+console.log(acc1);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());
+
+console.log(acc1);
+console.log(acc1._pin);
