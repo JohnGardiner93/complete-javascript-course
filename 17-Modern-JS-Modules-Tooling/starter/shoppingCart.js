3d8bee6c60c26a0faeacbf10c39bb148a6711250
John Gardiner <JohnGardiner93@users.noreply.github.com>
2021-03-26 14:36:13 -0400

    JS Coursework - 3/26/21
    Section 17
    - An Overview of Modules in JavaScript
    - STARTED Exporting and Importing in ES6 Modules

---
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,10 @@
+// Exporting Module
+console.log(`Exporting Module`);
+
+const shippingCost = 10;
+const cart = [];
+
+const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
